1bc729da9daa1f20e6feaf62450bac49c4317fa7
25DP3GSval <A251004GS@rvt.lv>
2026-05-12 13:33:04 +0300

    izmainiju failu uzdevums lai aprekinatu rinka linijas garumu izmantojot pi.

--- Uzdevums.py
+++ Uzdevums.py
@@ -1,8 +1,10 @@
+from math import pi
+
 radiuss = int(input("Ievadi Rādiusu:"))
 
 while radiuss < 0:
     radiuss = int(input("Ievadi pozitīvu rādiusu:"))
 
-risinajums = 2 * 3.1415 * radiuss
+risinajums = 2 * pi * radiuss
 
 print("Risinājums:", risinajums)
